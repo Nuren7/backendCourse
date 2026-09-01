113f91a12d20aa61c512e6dc4846ff4f4e47e6fc
Nuren7 <nurenislam01@gmail.com>
2026-09-01 14:49:54 +0100

    1

---
+++ src/server.js
@@ -0,0 +1,25 @@
+import express from "express";
+
+//Import Routes
+import movieRoutes from "./routes/movieRoutes.js";
+
+
+const app = express();
+
+
+//API Routes
+app.use("/movies", movieRoutes)
+
+
+const PORT = 5001;
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
+});
+
+//GET, POST, PUT, DELETE
+//http://localhost:5001
+
+//AUTH - Signin, Signup
+//MOVIE - GETTING ALL MOVIES
+//USER - Profile
+//WATCHLIST 
